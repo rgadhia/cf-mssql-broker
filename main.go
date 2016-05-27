db2c59d9fae1167e533b0b4a584fe8cead522fa0
Ravi Gadhia <rgadhia@pivotal.io>
2016-05-27 00:22:19 -0700

    changed repo

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/cloudfoundry-incubator/cf-mssql-broker/config"
-	"github.com/cloudfoundry-incubator/cf-mssql-broker/provisioner"
+	"github.com/rgadhia/cf-mssql-broker/config"
+	"github.com/rgadhia/cf-mssql-broker/provisioner"
 	"github.com/pivotal-cf/brokerapi"
 	"github.com/pivotal-golang/lager"
 )
